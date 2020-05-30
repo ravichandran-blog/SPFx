167455cb42c3ddd523ef412328150a01a1d61b8f
Ravichandran Krishnasamy <ravimca@live.com>
2020-05-31 03:35:37 +0530

    v1

--- spfx-pnp-logging/src/webparts/spfxPnpLogging/components/SpfxPnpLogging.module.scss.ts
+++ spfx-pnp-logging/src/webparts/spfxPnpLogging/components/SpfxPnpLogging.module.scss.ts
@@ -1,16 +1,7 @@
 /* tslint:disable */
 require("./SpfxPnpLogging.module.css");
 const styles = {
-  spfxPnpLogging: 'spfxPnpLogging_c61d1ae0',
-  container: 'container_c61d1ae0',
-  row: 'row_c61d1ae0',
-  column: 'column_c61d1ae0',
-  'ms-Grid': 'ms-Grid_c61d1ae0',
-  title: 'title_c61d1ae0',
-  subTitle: 'subTitle_c61d1ae0',
-  description: 'description_c61d1ae0',
-  button: 'button_c61d1ae0',
-  label: 'label_c61d1ae0'
+  spfxPnpLogging: 'spfxPnpLogging_87391fc9'
 };
 
 export default styles;
--- spfx-pnp-logging/src/webparts/spfxPnpLogging/components/SpfxPnpLogging.tsx
+++ spfx-pnp-logging/src/webparts/spfxPnpLogging/components/SpfxPnpLogging.tsx
@@ -1,63 +1,25 @@
 import * as React from 'react';
 import styles from './SpfxPnpLogging.module.scss';
-import { ISpfxPnpLoggingProps } from './ISpfxPnpLoggingProps';
-import { escape } from '@microsoft/sp-lodash-subset';
-import * as los from './Listener';
 import { PrimaryButton } from 'office-ui-fabric-react/lib/Button';
-import { autobind } from 'office-ui-fabric-react/lib/Utilities';
-
-import { ApplicationInsights } from '@microsoft/applicationinsights-web';
-import { ReactPlugin, withAITracking } from '@microsoft/applicationinsights-react-js';
-import { createBrowserHistory } from "history";
-import { AILogListenerService } from "./Listener";
 
 import {
   Logger,
-  ConsoleListener,
   LogLevel
 } from "@pnp/logging";
 
-
-
-const browserHistory = createBrowserHistory({ basename: '' });
-
-
-
-export default class SpfxPnpLogging extends React.Component<ISpfxPnpLoggingProps, {}> {
-  constructor(props: ISpfxPnpLoggingProps) {
-    super(props)
-    Logger.subscribe(new ConsoleListener());
-    Logger.subscribe(new AILogListenerService());
-
-  }
-  public render(): React.ReactElement<ISpfxPnpLoggingProps> {
+export default class SpfxPnpLogging extends React.Component<{}, {}> {
+  public render(): React.ReactElement<{}> {
     return (
       <div className={styles.spfxPnpLogging}>
-        <PrimaryButton onClick={this.btnclicked} text="nice"></PrimaryButton>
+        <PrimaryButton onClick={this.btnclicked} text="Tigger"></PrimaryButton>
       </div>
     );
   }
 
-  @autobind
   private btnclicked() {
-    // write logs a simple string as the message value of the LogEntry
-    Logger.write("This is logging a simple string");
-
-    // optionally passing a level, default level is Verbose
-    Logger.write("This is logging a simple string", LogLevel.Error);
-
-    // this will convert the object to a string using JSON.stringify and set the message with the result
-    Logger.writeJSON({ name: "value", name2: "value2" });
-
-    // optionally passing a level, default level is Verbose
-    Logger.writeJSON({ name: "value", name2: "value2" }, LogLevel.Warning);
-
-    // specify the entire LogEntry interface using log
-    Logger.log({
-      data: { name: "value", name2: "value2" },
-      level: LogLevel.Warning,
-      message: "This is my message"
-    });
+    Logger.write("This information triggerd from react component");
+    Logger.write("This warning triggerd from react component", LogLevel.Warning);
+    Logger.write("This error triggerd from react component", LogLevel.Error);
+    Logger.writeJSON({ FirstName: "Ravichandran", LastName: "Krishnasamy" }, LogLevel.Info);
   }
 }
-
