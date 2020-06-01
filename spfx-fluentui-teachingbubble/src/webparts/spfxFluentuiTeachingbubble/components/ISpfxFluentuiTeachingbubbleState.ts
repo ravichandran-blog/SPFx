a10631997b528c977c8aaf5cad82e86ef6d50748
Ravichandran Krishnasamy <ravimca@live.com>
2020-06-02 03:15:14 +0530

    v1

--- spfx-fluentui-teachingbubble/src/webparts/spfxFluentuiTeachingbubble/components/ISpfxFluentuiTeachingbubbleState.ts
+++
@@ -1,9 +0,0 @@
-import { IViewField } from "@pnp/spfx-controls-react/lib/ListView";
-
-export interface ISpfxFluentuiTeachingbubbleState {
-  items: any[];
-  viewFields: IViewField[];
-  bubble1: boolean;
-  bubble2: boolean;
-  bubble3: boolean;
-}
