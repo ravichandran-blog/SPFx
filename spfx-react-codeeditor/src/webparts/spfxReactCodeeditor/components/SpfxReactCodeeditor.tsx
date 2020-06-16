95e7718576cbb8881ffb4315f060c127c59dbe4e
Ravichandran Krishnasamy <ravimca@live.com>
2020-06-17 01:35:50 +0530

    v1

--- spfx-react-codeeditor/src/webparts/spfxReactCodeeditor/components/SpfxReactCodeeditor.tsx
+++ spfx-react-codeeditor/src/webparts/spfxReactCodeeditor/components/SpfxReactCodeeditor.tsx
@@ -1,72 +1,56 @@
 import * as React from 'react';
-// import styles from './SpfxReactCodeeditor.module.scss';
-// import { ISpfxReactCodeeditorProps } from './ISpfxReactCodeeditorProps';
-// import { escape } from '@microsoft/sp-lodash-subset';
-// import Editor from '@monaco-editor/react';
+import styles from './SpfxReactCodeeditor.module.scss';
+import { ISpfxReactCodeeditorProps } from './ISpfxReactCodeeditorProps';
+import Editor from 'react-coder';
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/theme-github";
 
 
+export interface ISpfxReactCodeeditorState {
+  code: string;
+}
 
-// export interface ISpfxReactCodeeditorState {
-//   code: string;
-// }
+export default class SpfxReactCodeeditor extends React.Component<ISpfxReactCodeeditorProps, ISpfxReactCodeeditorState> {
+  constructor(props: ISpfxReactCodeeditorProps, state: ISpfxReactCodeeditorState) {
+    super(props);
+    this.state = ({
+      code: `import React from "react";
+import ReactDOM from "react-dom";
 
-// export default class SpfxReactCodeeditor extends React.Component<ISpfxReactCodeeditorProps, ISpfxReactCodeeditorState> {
-//   constructor(props:ISpfxReactCodeeditorProps,state:ISpfxReactCodeeditorState)
-//   {
-//     super(props);
-//     this.state=({code:`import React from "react";
-//     import ReactDOM from "react-dom";
-
-//     function App() {
-//       return (
-//         <h1>Hello world</h1>
-//       );
-//     }
-
-//     ReactDOM.render(<App />, document.getElementById("root"));
-//     `})
-//   }
-//   public render(): React.ReactElement<ISpfxReactCodeeditorProps> {
-//     return (
-//       <div className={ styles.spfxReactCodeeditor }>
-
-//         <Editor height="90vh" language="javascript" />;
-//       </div>
-//     );
-//   }
-// }
-
-import { useRef, useState } from "react";
-
-import Editor from "@monaco-editor/react";
-
-function SpfxReactCodeeditor() {
-  const [isEditorReady, setIsEditorReady] = useState(false);
-  const valueGetter = useRef();
-
-  function handleEditorDidMount(_valueGetter) {
-    setIsEditorReady(true);
-    valueGetter.current = _valueGetter;
-  }
-
-  function handleShowValue() {
-    //alert(valueGetter.current());
-  }
-
+function App() {
   return (
-    <>
-      <button onClick={handleShowValue} disabled={!isEditorReady}>
-        Show value
-      </button>
-
-      <Editor
-        height="90vh"
-        language="javascript"
-        value={"// write your code here"}
-        editorDidMount={handleEditorDidMount}
-      />
-    </>
+    <h1>Hello world</h1>
   );
 }
 
-export default SpfxReactCodeeditor;
+ReactDOM.render(<App />, document.getElementById("root"));
+    `})
+  }
+  public render(): React.ReactElement<ISpfxReactCodeeditorProps> {
+    return (
+      <div className={styles.spfxReactCodeeditor}>
+        <AceEditor
+          placeholder="Placeholder Text"
+          mode="javascript"
+          theme="monokai"
+          name="blah2"
+          onChange={(code) => this.setState({ code })}
+          fontSize={14}
+          showPrintMargin={true}
+          showGutter={true}
+          highlightActiveLine={true}
+          value={this.state.code}
+          setOptions={{
+            enableBasicAutocompletion: true,
+            enableLiveAutocompletion: true,
+            enableSnippets: true,
+            showLineNumbers: true,
+            tabSize: 2,
+          }} />
+
+      </div>
+    );
+  }
+}
