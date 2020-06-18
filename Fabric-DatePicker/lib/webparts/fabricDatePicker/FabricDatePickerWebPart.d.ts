fac1398825d7aedcf7144caf84d4a4ed86d0c7de
Ravichandran Krishnasamy <ravimca@live.com>
2020-06-18 14:29:11 +0530

    v1

--- Fabric-DatePicker/lib/webparts/fabricDatePicker/FabricDatePickerWebPart.d.ts
+++
@@ -1,5 +0,0 @@
-import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
-import { IFabricDatePickerWebpartProps } from './components/IFabricDatePickerProps';
-export default class FabricPeoplePickerWebPart extends BaseClientSideWebPart<IFabricDatePickerWebpartProps> {
-    render(): void;
-}
