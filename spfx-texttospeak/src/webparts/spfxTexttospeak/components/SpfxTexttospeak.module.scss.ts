146cb6102afab34d9c9d14bcedf6fda2faa0cc49
Ravichandran Krishnasamy <ravimca@live.com>
2020-06-26 01:31:01 +0530

    v1

---
+++ spfx-texttospeak/src/webparts/spfxTexttospeak/components/SpfxTexttospeak.module.scss.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+require("./SpfxTexttospeak.module.css");
+const styles = {
+  spfxTexttospeak: 'spfxTexttospeak_380e50b2'
+};
+
+export default styles;
+/* tslint:enable */
--- spfx-texttospeak/src/webparts/spfxTexttospeak/components/SpfxTexttospeak.tsx
+++ spfx-texttospeak/src/webparts/spfxTexttospeak/components/SpfxTexttospeak.tsx
@@ -1,25 +1,126 @@
 import * as React from 'react';
 import styles from './SpfxTexttospeak.module.scss';
 import { ISpfxTexttospeakProps } from './ISpfxTexttospeakProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import { ISpfxTexttospeakState } from './ISpfxTexttospeakState';
+import Speech from 'speak-tts'
+import { TextField, DefaultButton, Stack, IStackTokens } from 'office-ui-fabric-react/lib';
 
-export default class SpfxTexttospeak extends React.Component<ISpfxTexttospeakProps, {}> {
+const sectionStackTokens: IStackTokens = { childrenGap: 10 };
+export default class SpfxTexttospeak extends React.Component<ISpfxTexttospeakProps, ISpfxTexttospeakState> {
+  private speech: Speech;
+  constructor(props: ISpfxTexttospeakProps, state: ISpfxTexttospeakState) {
+    super(props);
+    this.state = {
+      textcontent: ''
+    };
+    
+    this.speech = new Speech();
+    this.speech
+      .init({
+        volume: 1,
+        lang: 'en-GB',
+        rate: 1,
+        pitch: 1,
+        'voice': 'Google UK English Male',
+        //'splitSentences': false,
+        listeners: {
+          onvoiceschanged: voices => {
+            console.log("Voices changed", voices);
+          }
+        }
+      })
+      .then(data => {
+        console.log("Speech is ready", data);
+      })
+      .catch(e => {
+        console.log("An error occured while initializing : ", e);
+      });
+  }
+  
   public render(): React.ReactElement<ISpfxTexttospeakProps> {
     return (
-      <div className={ styles.spfxTexttospeak }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
+      <div className={styles.spfxTexttospeak}>
+        <TextField rows={10} label="Text content" multiline autoAdjustHeight onChange={(e, newval) => this.setState({ textcontent: newval })} />
+        <br />
+        <Stack horizontal tokens={sectionStackTokens}>
+          <DefaultButton
+            text={'Play'}
+            allowDisabledFocus onClick={this.onclickPlay} />
+          <DefaultButton
+            text={'Stop'}
+            allowDisabledFocus onClick={this.onclickStop} />
+        </Stack>
       </div>
     );
+  }
+
+  private onclickPause = (): void => {
+    this.speech
+      .speak({
+        text: this.state.textcontent,
+        queue: false,
+        listeners: {
+          onstart: () => {
+            console.log("Start utterance");
+          },
+          onend: () => {
+            console.log("End utterance");
+          },
+          onresume: () => {
+            console.log("Resume utterance");
+          },
+          onboundary: event => {
+            console.log(
+              event.name +
+              " boundary reached after " +
+              event.elapsedTime +
+              " milliseconds."
+            );
+          }
+        }
+      })
+      .then(data => {
+        console.log("Success !", data);
+      })
+      .catch(e => {
+        console.error("An error occurred :", e);
+      });
+  };
+
+  private onclickStop = (): void => {
+    this.speech.cancel();
+  };
+
+  private onclickPlay = (): void => {
+    this.speech.speak({
+      text: this.state.textcontent,
+      queue: false,
+      listeners: {
+        onstart: () => {
+          console.log("Start utterance");
+        },
+        onend: () => {
+          console.log("End utterance");
+        },
+        onresume: () => {
+          console.log("Resume utterance");
+
+        },
+        onboundary: event => {
+          console.log(
+            event.name +
+            " boundary reached after " +
+            event.elapsedTime +
+            " milliseconds."
+          );
+        }
+      }
+    })
+      .then(data => {
+        console.log("Success !", data);
+      })
+      .catch(e => {
+        console.error("An error occurred :", e);
+      });
   }
 }
