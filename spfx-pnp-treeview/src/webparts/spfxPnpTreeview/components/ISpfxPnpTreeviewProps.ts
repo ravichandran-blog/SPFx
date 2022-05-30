882955db42a56fc463fc8581dea030d8b123f048
Ravichandran Krishnasamy <ravimca@live.com>
2022-05-31 05:25:43 +0530

    Delete spfx-pnp-treeview directory

--- spfx-pnp-treeview/src/webparts/spfxPnpTreeview/components/ISpfxPnpTreeviewProps.ts
+++
@@ -1,4 +0,0 @@
-export interface ISpfxPnpTreeviewProps {
-  description: string;
-  context: any | null;
-}
--- spfx-pnp-treeview/src/webparts/spfxPnpTreeview/components/ISpfxPnpTreeviewState.ts
+++
@@ -1,5 +0,0 @@
-import { ITreeItem } from "@pnp/spfx-controls-react/lib/TreeView";
-
-export interface ISpfxPnpTreeviewState {
-  TreeLinks: ITreeItem[];
-}
